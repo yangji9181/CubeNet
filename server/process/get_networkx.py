88d5927bd8f587868b699942e2448e2b7183b5dc
Dai Teng <tengdai722@gmail.com>
2019-05-02 12:04:04 -0500

    deal with disconnected graph

--- server/process/get_networkx.py
+++ server/process/get_networkx.py
@@ -6,5 +6,6 @@ def get_graph_property(filename):
     with open(filename) as f:
         js_graph = json.load(f)
     G = json_graph.node_link_graph(js_graph)
+    G = max(nx.connected_component_subgraphs(G), key=len)
     return (nx.radius(G), nx.diameter(G), nx.density(G))
 
